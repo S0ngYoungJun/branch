f02630ad4b75f8d5c0b08718ab13080c883d8e36
songyoungjun <clesaaaaaa@gmail.com>
2023-10-12 10:43:06 +0900

    기본 서버 제작

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('hell')
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+});
+
+server.litsen(3500, (err) => {
+  if(!err){
+    console.error('서버 안돌아감');
+
+  }else{
+    console.log("3500포토에 돌아감 http://localhsost:3500")
+  }
+})
