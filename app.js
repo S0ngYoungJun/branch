98493fd2d9a5f9d8d22ab3e0340d98be7609e9ae
songyoungjun <clesaaaaaa@gmail.com>
2023-10-12 10:57:11 +0900

    향마루 응답처리

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-
+ res.writeHead(200,{"Content-Type":"text/plain"})
+ res.write("향마루");
+ res.end();
 });
 
-server.litsen(3500, (err) => {
+server.listen(3500, (err) => {
   if(!err){
     console.error('서버 안돌아감');
 
